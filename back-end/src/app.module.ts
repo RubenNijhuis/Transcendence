98ea496c22fe9d1ac6e1ee77fd7e0f0add7a62ef
Zeno Tan <zenotan@ztan.local>
2022-08-10 17:06:08 +0200

    adding user resource

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+import { TypeOrmConfigService } from './typeorm/typeorm.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService })],
+  imports: [TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
