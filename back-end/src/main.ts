a7478a9d6a20b5c8c7fe453b7a16bdd81a3bae87
Zeno Tan <ztan@f1r1s3.codam.nl>
2022-08-23 11:42:36 +0200

    cleanup

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -3,12 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { env } from 'node:process';
+import * as session from 'express-session';
 import passport from 'passport';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app: NestExpressApplication = await NestFactory.create(AppModule);
     const port = Number(env.PORT);
 
+    const configService = app.get(ConfigService);
     // Enable cors so the front-end can communicate
     app.enableCors({
         origin: 'http://localhost:8080',
@@ -18,6 +21,18 @@ async function bootstrap() {
     app.useGlobalPipes(
         new ValidationPipe({ whitelist: true, transform: true }),
     );
+    
+    app.setGlobalPrefix('api');
+    app.use(session({
+    cookie: {
+            maxAge: 60000 * 60 * 24,
+        },
+        secret: "djfhjdhfdjkhfjkhsjk",
+        resave: false,
+        saveUninitialized: false,
+    }))
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     await app.listen(port, () => {
         console.log('[WEB]', String(env.BASE_URL));
