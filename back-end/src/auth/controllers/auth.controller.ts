ddacc748619e7c740cc8d773a58743397ff8c9e7
Ruben Nijhuis <contact@rubennijhuis.com>
2022-09-05 17:20:44 +0200

    feat: AUTH [WIP]

--- back-end/src/auth/controllers/auth.controller.ts
+++ back-end/src/auth/controllers/auth.controller.ts
@@ -1,40 +1,50 @@
-import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
-import { ADDRCONFIG } from 'dns';
-import { Response } from 'express';
-import { FortyTwoAuthGuard } from 'src/auth/guard';
-import { User } from 'src/typeorm';
-import { AuthService } from '../services/auth.service';
+import { HttpService } from "@nestjs/axios";
+import { Controller, Get, Post, Query, Res, UseGuards } from "@nestjs/common";
+import { ADDRCONFIG } from "dns";
+import { Response } from "express";
+import { FortyTwoAuthGuard } from "src/auth/guard";
+import { User } from "src/typeorm";
+import { AuthService } from "../services/auth.service";
 
-@Controller('auth')
+import Axios from "axios";
+
+@Controller("auth")
 export class AuthController {
-  @Get('login')
-  @UseGuards(FortyTwoAuthGuard)
-  login() {}
+    constructor(private authService: AuthService) {}
 
-  @Get('42/callback')
-  @UseGuards(FortyTwoAuthGuard)
-  redirect(@Res() res: Response) {
-    res.sendStatus(200);
-  }
+    // @UseGuards(FortyTwoAuthGuard)
+    @Get("login?")
+    login(@Query("token") token: string) {
+        console.log("TOKEHN: ", token);
+        grant_type = client_credentials
+        
+        client_id = MY_AWESOME_UID
+        client_secret = MY_AWESOME_SECRET"
 
-  @Get('status')
-  status() {}
+        Axios.post("https://api.intra.42.fr/oauth/authorize", {
+            grant_type: 
+        });
+    }
 
-  @Get('logout')
-  logout() {}
+    @Get("42/callback")
+    @UseGuards(FortyTwoAuthGuard)
+    redirect(@Res() res: Response) {
+        res.sendStatus(200);
+    }
 
-  constructor (private authService: AuthService) {}
+    @Get("status")
+    status() {}
 
-  @Post('singup')
-  signup() {
-    return this.authService.signup;
-  }
+    @Get("logout")
+    logout() {}
 
-  @Post('singin')
-  signin() {
-    return this.authService.signin;
-  }
+    @Post("singup")
+    signup() {
+        return this.authService.signup;
+    }
 
+    @Post("singin")
+    signin() {
+        return this.authService.signin;
+    }
 }
-
-
