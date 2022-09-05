00f20f891accc0f7f89219194679114db745f42b
Zeno Tan <zenotan@ztan.local>
2022-09-05 17:21:29 +0200

    intermediate

--- back-end/src/auth/controllers/auth.controller.ts
+++ back-end/src/auth/controllers/auth.controller.ts
@@ -1,20 +1,32 @@
-import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { ADDRCONFIG } from 'dns';
 import { Response } from 'express';
 import { FortyTwoAuthGuard } from 'src/auth/guard';
 import { User } from 'src/typeorm';
+import { ConfirmDto } from '../dto/confirm.dto';
 import { AuthService } from '../services/auth.service';
+import { HttpService } from '@nestjs/axios';
 
 @Controller('auth')
 export class AuthController {
+  constructor (private authService: AuthService) {}
+
   @Get('login')
   @UseGuards(FortyTwoAuthGuard)
-  login() {}
+  login() {
 
-  @Get('42/callback')
-  @UseGuards(FortyTwoAuthGuard)
-  redirect(@Res() res: Response) {
-    res.sendStatus(200);
+  }
+
+  // @Get('42/callback')
+  // // @UseGuards(FortyTwoAuthGuard)
+  // redirect(@Res() res: Response) {
+  //   res.sendStatus(200);
+  // }
+
+  @Post('confirm')
+  // @UseGuards(FortyTwoAuthGuard)
+  confirm(@Body() confirmDto: ConfirmDto) {
+    const ret = 
   }
 
   @Get('status')
@@ -22,8 +34,6 @@ export class AuthController {
 
   @Get('logout')
   logout() {}
-
-  constructor (private authService: AuthService) {}
 
   @Post('singup')
   signup() {
