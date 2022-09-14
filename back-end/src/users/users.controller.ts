e4b97d1c286526f5293ac12bbc559f4e513f1979
Zeno Tan <zenotan@ztan.local>
2022-09-14 10:47:19 +0200

    prepare for merge

--- back-end/src/users/users.controller.ts
+++ back-end/src/users/users.controller.ts
@@ -2,16 +2,23 @@ import { HttpService } from "@nestjs/axios";
 import {
     Body,
     Controller,
+    FileTypeValidator,
     Get,
+    MaxFileSizeValidator,
+    ParseFilePipe,
     Post,
     Res,
+    UploadedFile,
     UsePipes,
     ValidationPipe
 } from "@nestjs/common";
+import { MulterModule } from "@nestjs/platform-express";
 import { seederConfig } from "src/configs/seeder.config";
 import { UserSeeder } from "src/database/seeds/user-create.seed";
 import { CreateUserDto } from "src/users/dtos/create-users.dto";
 import { UsersService } from "src/users/users.service";
+import { uploadImgDto } from "./dtos/upload-img.dto";
+const multer  = require('multer')
 
 @Controller("users")
 export class UsersController {
@@ -41,6 +48,34 @@ export class UsersController {
         } catch (error) {
             return error;
         }
+    }
+
+    @Post('upload-img')
+    async uploadImg(@Body() upload: uploadImgDto,
+        @UploadedFile( new ParseFilePipe({
+                validators: [
+                    new MaxFileSizeValidator({
+                        maxSize: 1000
+                    }),
+                    new FileTypeValidator({
+                        fileType: 'jpeg'
+                }),
+                ],
+            })
+        ) file: Express.Multer.File
+    ) {
+        const storage = multer.diskStorage({
+            destination: function (req, file, cb) {
+                cb(null, '/src/uploads')
+            },
+            filename: function (req, file, cb) {
+                const uniqueSuffix = upload.intraId // get this from jwt
+                cb(null, file.fieldname + '-' + uniqueSuffix)
+            }
+        })
+        const ret = MulterModule.register({
+            dest: 'src/uploads' + upload.type,
+        })
     }
 
     @Get('seeder')
