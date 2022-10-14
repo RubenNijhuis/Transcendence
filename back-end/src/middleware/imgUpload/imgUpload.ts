f8aa847360023e56e65f19a033715bfbc54c3554
Zeno Tan <zenotan@ztan.home>
2022-10-14 14:29:58 +0200

    updated image upload to handle png, jpg, jpeg and webm

--- back-end/src/middleware/imgUpload/imgUpload.ts
+++ back-end/src/middleware/imgUpload/imgUpload.ts
@@ -1,68 +1,26 @@
-import { Inject, Injectable, NestInterceptor, Type } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { FileInterceptor } from "@nestjs/platform-express";
 import { MulterOptions } from "@nestjs/platform-express/multer/interfaces/multer-options.interface";
-import { InjectRepository, TypeOrmModule } from "@nestjs/typeorm";
+import { readdirSync, unlink, unlinkSync } from "fs";
 import multer, { diskStorage } from "multer";
-import { User } from "src/entities";
-import { UserService } from "src/services/user/user.service";
-import { Repository } from "typeorm";
 
-// @Injectable()
-// export class ImgUpload {
-//     inject: [UserService];
-//     constructor(
-//         private readonly userService: UserService
-//     ) {}
+export const deleteFiles = (path, id, uploadedFile) => {
+    const files = readdirSync(path).filter(file => file.startsWith(`${id}.`));
+    if (files) {
+      const filteredFiles = files.filter(e => (!e.includes(uploadedFile)));
+      if (filteredFiles) {
+        for (let i = 0; i < filteredFiles.length; i++) {
+          unlinkSync(path + filteredFiles[i])
+        }
+      }
+    }
+}
 
-//     private async getFileName(req, file, cb): Promise<any> {
-//         const extention: string = file.originalname.split('.').reverse()[0];
-//         const userID: string = await this.userService.getUserIdFromIntraId(req.user.intraID);
-//         return cb(null, userID + '.' + extention);
-//     }
-
-//     private imgFilter = (req, file, cb) => {
-//         if (!file.originalname.match(/\.(png|jpg|jpeg|webm)$/)) { 
-//             return cb(new Error('Please upload a Image'))
-//         }
-//         cb(null, true)
-//     };
-
-//     bannerOptions(): MulterOptions {
-//         const storage = diskStorage({
-//             destination: './upload/banner',
-//             filename: this.getFileName,
-//         });
-//         const options: MulterOptions = {
-//             fileFilter: this.imgFilter,
-//             storage: storage
-//         };
-
-//         return options
-//     };
-
-//     profilePicOptions(): MulterOptions {
-//         const storage = diskStorage({
-//             destination: './upload/profile',
-//             filename: this.getFileName,
-//         });
-//         const options: MulterOptions = {
-//             fileFilter: this.imgFilter,
-//             storage: storage
-//         };
-
-//         return options
-//     };
-// };
-export const getFileName = async (req, file, cb) => {
-    // const UserServ = new UserService(Repository<User>, ConfigService);
+export const getFileName = (req, file, cb) => {
     const extention: string = file.originalname.split('.').reverse()[0];
-    // const userID: string = await UserService.getUserIdFromIntraId(req.user.intraID);
     cb(null, req.user.intraID + '.' + extention);
 };
 
 export const imgFilter = (req, file, cb) => {
-    if (!file.originalname.match(/\.(jpg)$/)) { 
+    if (!file.originalname.match(/\.(png|jpg|jpeg|webm)$/)) { 
         return cb(new Error('Please upload a Image'))
     }
     cb(null, true)
@@ -87,13 +45,3 @@ export const profileOptions: MulterOptions = {
     fileFilter: imgFilter,
     storage: profileStorage
 }
-
-export const testcase = () => {
-    return {
-        fileFilter: imgFilter,
-        storage: {
-            destination: './upload/profile',
-            filename: getFileName,
-        }
-    };
-};
