fab85384d3fa170aeb371b7b8e138480e1ddbab3
Ruben Nijhuis <contact@rubennijhuis.com>
2022-11-11 13:50:23 +0100

    Profile settings update

--- front-end/src/containers/Settings/ProfileSettings/Description/UploadDescription.tsx
+++ front-end/src/containers/Settings/ProfileSettings/Description/UploadDescription.tsx
@@ -5,43 +5,47 @@ import { CreateForm, StyledInput } from "./UploadDescription.style";
 import ApiRoutes from "../../../../config/ApiRoutes";
 
 // DEBUG
-import { MutableRefObject, useRef} from "react";
 import { useUser } from "../../../../contexts/UserContext";
 import { API } from "../../../../proxies/instances/apiInstance";
-
+import { useFormInput } from "../../../../components/Form/hooks";
 
 const UploadDescription = () => {
-	const { user } = useUser();
-	const ref = useRef() as MutableRefObject<HTMLTextAreaElement>;
+    const description = useFormInput("");
 
-	const handleText = async (event: any) => {
-		try {
-			const route = ApiRoutes.updateDescription(); //moet dit als een aparte proxie of mag ook zo?
-			const username = user.username;
-			const description = ref.current.value;
-			const config = {
-				username,
-				description
-			}
-			const { data } = await API.post(route, config);
-			return Promise.resolve(data);
+    ////////////////////////////////////////////////////////////
 
-		} catch (err) {
-			return Promise.reject(err);
-		}
+    const { user } = useUser();
 
-	};
+    ////////////////////////////////////////////////////////////
+
+    const handleText = async (event: any) => {
+        try {
+            const route = ApiRoutes.updateDescription(); // TODO: make proxy
+            const username = user.username;
+
+            const config = {
+                username,
+                description: description.value
+            };
+
+            const { data } = await API.post(route, config);
+
+            return Promise.resolve(data);
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    };
 
     return (
         <CreateForm>
-                    <StyledInput>
-                        <label>Change description</label>
-                        {/* {error && <ErrorMessage message={error} />} */}
-                        <textarea ref={ref} />
-                    </StyledInput>
-					<button onClick={handleText}>Click</button>
+            <StyledInput>
+                <label>Change description</label>
+                {/* {error && <ErrorMessage message={error} />} */}
+                <textarea {...description} rows={4} />
+            </StyledInput>
+            <button onClick={handleText}>Click</button>
         </CreateForm>
     );
-}
+};
 
 export default UploadDescription;
