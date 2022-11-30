1d1106b03a93df70489bf8306fffe434737dc0e4
ztan <ztan@student.codam.nl>
2022-11-30 16:38:30 +0100

    redid some naming things because git can be a real pain

---
+++ front-end/src/types/Profile.ts
@@ -0,0 +1,25 @@
+namespace Profile {
+    export type ProfileID = string;
+  
+    export interface Instance {
+      uid: ProfileID;
+  
+      username: string;
+      description: string;
+  
+      img_url: string;
+      banner_url: string;
+  
+      color: string;
+  
+      isTfaEnabled?: boolean;
+  
+      rank: number;
+      wins: number;
+      losses: number;
+    }
+  }
+  
+  ////////////////////////////////////////////////////////////
+  
+  export default Profile;
