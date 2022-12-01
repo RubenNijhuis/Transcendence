dc2568d88651136bc3cfefbf1462cf64963174d7
ztan <ztan@student.codam.nl>
2022-12-01 15:48:19 +0100

    WIP: [logout]

--- front-end/src/components/DropDown/DropDown.tsx
+++ front-end/src/components/DropDown/DropDown.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { FiSettings } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import PageRoutes from "../../config/PageRoutes";
+import { signOut } from "../../contexts/AuthContext/AuthContext.bl";
 import { Container, List } from "./DropDown.style";
 
 const DropDown = () => {
@@ -20,7 +21,7 @@ const DropDown = () => {
                         <Link to={PageRoutes.settings} className="link" >{"settings"}</Link>
                     </li>
                     <li key="logout">
-                        <Link to={PageRoutes.settings} className="link">{"logout"}</Link>
+                        <Link onClick={signOut} to={PageRoutes.home} className="link" >{"logout"}</Link>
                     </li>
                 </List>
             ): null}
